20999e9a972d8c6573fac0ce3bad2c43566792a5
juacabvil <juacabvil@alum.us.es>
2023-01-03 21:41:31 +0100

    Inicio index + inicio models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Juego(models.Model):
+    name = models.CharField(max_length=255)
+    mods = models.IntegerField()
+    downloads = models.IntegerField()
+
+class Mod(models.Model):
+    name = models.CharField(max_length=255)
+    last_update = models.DateField()
+    uploader = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    downloads = models.IntegerField()
+    categories = models.CharField(max_length=255)
+    game = models.ForeignKey(Juego, on_delete=models.CASCADE)
+
+
--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from app import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
 ]
 
